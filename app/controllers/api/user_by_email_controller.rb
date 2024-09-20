578a7035ab5364871974ae9b11942364a186f3e3
sean <seanl20@yahoo.com>
2024-09-20 14:31:50 +1000

    feat: create new controller for user by email

---
+++ app/controllers/api/user_by_email_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Api
+  class UserByEmailController < ApplicationController
+    def show
+      @user = Repositories::UserRepo.new.get_by_email(email: params[:email])
+
+      respond_to do |format|
+        format.json do
+          render json: @user.to_json, status: :ok
+        end
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      respond_to do |format|
+        format.json do
+          render json: { error: e.message}.to_json, status: :not_found
+        end
+      end
+    end
+  end
+end
--- spec/integration/repositories/user_repo_spec.rb
+++ spec/integration/repositories/user_repo_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Repositories::UserRepo do
     subject(:get) { described_class.new.get(id: user_id) }
 
     context "user exists" do
-      let(:user) { FactoryBot.create(:user) }
+      let!(:user) { FactoryBot.create(:user) }
       let(:user_id) { user.id }
 
       it "is successful" do
@@ -20,5 +20,27 @@ RSpec.describe Repositories::UserRepo do
         expect{ get }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
+  end
+
+  describe "#get_by_email" do
+  subject(:get_by_email) { described_class.new.get_by_email(email: email) }
+
+  context "user exists" do
+    let!(:user) { FactoryBot.create(:user, email: "test@example.com") }
+    let(:email) { "test@example.com" }
+
+    it "is successful" do
+      expect(get_by_email).to eq(user)
+    end
+  end
+
+  context "user does not exists" do
+    let(:email) { "test@example.com" }
+
+    it "is not found" do
+      expect{ get_by_email }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
   end
 end
