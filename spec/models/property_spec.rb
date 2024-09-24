75b0e8ed40c3bc6c4decae3e19e9beb40d56dda6
sean <seanl20@yahoo.com>
2024-09-24 17:51:23 +1000

    feat: add property model

---
+++ spec/models/property_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Property, type: :model do
+  context "validates" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:headline) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:address_1) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:country) }
+  end
+end
