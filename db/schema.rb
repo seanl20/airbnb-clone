1567cf76019e7d1c30f6518c85933cb12853e913
sean <seanl20@yahoo.com>
2024-09-25 18:01:25 +1000

    feat: add geocoding to property

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_24_073416) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_25_075034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_073416) do
     t.datetime "updated_at", null: false
     t.string "address_1"
     t.string "address_2"
+    t.float "latitude"
+    t.float "longitude"
+    t.index ["latitude", "longitude"], name: "index_properties_on_latitude_and_longitude"
   end
 
   create_table "users", force: :cascade do |t|
