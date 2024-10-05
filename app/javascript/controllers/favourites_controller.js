ea84ea76584b26fba17d2cf1a7c7319e332d30e3
sean <seanl20@yahoo.com>
2024-10-05 13:46:23 +1000

    feat: implemented api calls to favourite and unfavourite properties

--- app/javascript/controllers/favourites_controller.js
+++ app/javascript/controllers/favourites_controller.js
@@ -1,14 +1,36 @@
 import { Controller } from "@hotwired/stimulus"
+import axios from 'axios';
 
 export default class extends Controller {
 
   favourite(){
+    if (this.element.dataset.userLoggedIn === "false") {
+      return document.querySelector('[data-header-target="userAuthLink"]').click();
+    }
+
     if ( this.element.dataset.favourited === "true"){
-      this.element.dataset.favourited = "false"
-      this.element.setAttribute('fill', '#CED4DA');
+      axios.delete(this.element.dataset.unfavouriteUrl, {
+        headers: { 
+          'ACCEPT': 'application/json'
+        }
+      })
+      .then((response) => {
+        this.element.dataset.favourited = "false"
+        this.element.setAttribute('fill', '#CED4DA');
+      });
     } else {
-      this.element.dataset.favourited = "true"
-      this.element.setAttribute('fill', 'red');
+      axios.post(this.element.dataset.favouriteUrl, {
+        user_id: this.element.dataset.userId,
+        property_id: this.element.dataset.propertyId
+      },{
+        headers: { 
+          'ACCEPT': 'application/json'
+        }
+      })
+      .then((response) => {
+        this.element.dataset.favourited = "true"
+        this.element.setAttribute('fill', 'red');
+      });
     }
   }
 }
