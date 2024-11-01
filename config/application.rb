5cbc586ed96439fa28e7a4e52a67c2eaadbaf758
sean <seanl20@yahoo.com>
2024-11-01 12:43:35 +1000

    feat: implement stripe

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,9 @@ module AirbnbClone
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    #Stripe Key
+    config.stripe.publishable_key = ENV["STRIPE_PUBLISHABLE_KEY"]
+    config.stripe.secret_key = ENV["STRIPE_SECRET_KEY"]
   end
 end
